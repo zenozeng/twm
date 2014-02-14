265cb08713a221aee9236a920ba30d7ca04c67b6
Zeno Zeng <zenoofzeng@gmail.com>
2014-02-14 17:01:09 +0800

    remove cp .twm/twm.js in compile.sh, fixes #23

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@ coffee --output . -bc src/*.coffee
 coffee --output ./api -bc src/api/*.coffee 
 coffee --output ./layouts -bc src/layouts/*.coffee
 coffee --output ./config -bc src/config/*.coffee
-cp config/config-sample.js ~/.twm/twm.js
+# cp config/config-sample.js ~/.twm/twm.js
 nohup gnome-shell --replace > ../log &
